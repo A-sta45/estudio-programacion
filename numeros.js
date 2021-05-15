068d314616b3ea2872d63bb826ca880a35c8db6f
Thomas Solano <thomassolanoperez0411@gmail.com>
2021-05-14 22:31:30 -0500

    nueva lecion numeros

---
+++ numeros.js
@@ -0,0 +1,65 @@
+let miNumero = 12;
+let miDecimal = 12.34;
+let miExponente =1e3;
+let miMilesima = 1e-3;
+
+console.log(miMilesima)
+
+const PI = 3.1415;
+let radio = 10;
+let diametro=30;
+let altura=25;
+let base =45;
+
+let sumar=0;
+sumar =radio+altura;
+console.log(sumar);
+
+//Multiplicar * divir / Restar - Modulo %
+
+let area= base*altura;
+console.log(area);
+
+console.log(10%5);
+
+// opreaciones logicas:
+
+area = 25;
+console.log(area)
+
+area ++;
+console.log(area)
+// -- 
+area *=2;
+console.log(area)
+
+area = area / 4;
+area /=4;
+
+// caracteres string
+
+let miEntradaNumerica = "23";
+let miSuma=1;
+let resultado = miEntradaNumerica+miSuma;
+console.log(typeof resultado)
+
+console.log(typeof parseInt(resultado))
+
+//
+let myIntNumber = 234;
+console.log(parseInt(myIntNumber));
+myIntNumber = "234";
+console.log(parseInt(myIntNumber));
+myIntNumber = "234.234";
+console.log(parseInt(myIntNumber));
+myIntNumber = "100 years";
+console.log(parseInt(myIntNumber));
+
+let myFloatNumber = 23.6712;
+console.log(parseFloat(myFloatNumber));
+myFloatNumber = "23.6712";
+console.log(parseFloat(myFloatNumber));
+myFloatNumber = "23,6712";
+console.log(parseFloat(myFloatNumber));
+myFloatNumber = "23.67 mts";
+console.log(parseFloat(myFloatNumber));
---
+++ taller2.js
@@ -0,0 +1,17 @@
+//prompt("Prueba")
+//utilizar promt para obtener 2 valores y multiplicarlos. Mostrar el resultado usando un console.log.
+//prompt('numero 1')
+//prompt('numero 2')
+// let     NumeroUno =parseInt(prompt('numero 1'))
+// let NumeroDos =parseInt(prompt('numero 2')) 
+
+// let resultado = NumeroUno + NumeroDos
+
+// console.log(resultado)
+
+// Ejercicio 2: Recibir 2 numeros una base y una altura para calcular el area de un triangulo.
+
+let base =parseInt(prompt('digite la base'))
+let altura =parseInt(prompt('digite la altura'))
+let area =base * altura / 2
+console.log(area)
